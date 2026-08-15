97fab31d59375f2655580578a511c7f817aa9ce0
matheshwarp-cyber <matheswar003@gmail.com>
2026-08-15 10:48:57 +0530

    First

---
+++ script.js
@@ -0,0 +1,42 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+// Open mobile menu
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close mobile menu
+menuCloseButton.addEventListener("click", () => {
+    menuOpenButton.click();
+});
+
+// Initialize Swiper
+const swiper = new Swiper(".slider-wrapper", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
